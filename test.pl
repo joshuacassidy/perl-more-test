2567ab987e53d1dd64ce569bc844c30a0c308302
joshuacassidy <joshcassidy79@gmail.com>
2020-05-21 18:34:34 +0100

    first commit

---
+++ test.pl
@@ -0,0 +1,14 @@
+use strict;
+use warnings;
+
+use Test::More tests => 1;
+
+subtest "Test 1: 1==1" => sub {
+
+    ok(1 == 1, "1==1");
+    ok("hello" eq "hello", "hello==hello");
+
+   
+    
+};
+ 
